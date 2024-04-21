a848b3a605c128579e546f2d4621f2764ee1f139
Jonathan Good <github@bungogood.com>
2024-04-21 16:21:36 +0100

    Adding stats

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::{
     io::{self, BufReader, BufWriter, Read},
     path::PathBuf,
     thread,
+    time::Duration,
 };
 
 use clap::Parser;
@@ -10,7 +11,7 @@ use crossbeam_channel;
 use num_format::{Locale, ToFormattedString};
 use tempfile::NamedTempFile;
 
-use orsay::{reader, worker, writer};
+use orsay::{reader, solver::Stats, worker, writer};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -63,38 +64,51 @@ fn main() -> io::Result<()> {
     let worker_threads = worker::start_workers(num_workers, read_rx, write_tx);
     let writer_thread = writer::start_writer(buf_writer, write_rx);
 
-    let mut num_puzzles = 0;
+    let mut stats = Stats::new();
     reader_thread.join().expect("Error joining reader thread");
     for worker in worker_threads {
-        num_puzzles += worker.join().expect("Error joining worker thread");
+        stats.add(&worker.join().expect("Error joining worker thread"));
     }
     writer_thread.join().expect("Error joining writer thread");
 
     if args.verbose {
-        let time_taken = start.elapsed();
         let outpath = match temp_file.as_ref() {
             Some(temp_file) => temp_file.path().to_path_buf(),
             None => args.outfile.unwrap(),
         };
-        let out_bytes = std::fs::read(outpath)?;
-
-        let sudokus_rate = num_puzzles as f64 / time_taken.as_secs_f64();
-        let avg_time = time_taken / num_puzzles as u32;
-        // let guess_rate = guesses as f32 / num_puzzles as f32;
-
-        let sha256sum = crypto_hash::hex_digest(crypto_hash::Algorithm::SHA256, &out_bytes);
-        // let md5sum = crypto_hash::hex_digest(crypto_hash::Algorithm::MD5, &out_bytes);
-
-        println!("Number of puzzles: {}", num_puzzles);
-        println!(
-            "Time Taken: {:.2?}, Speed: {}/s, Avg: {:.2?}",
-            time_taken,
-            (sudokus_rate as u32).to_formatted_string(&Locale::en),
-            avg_time
-        );
-        println!("SHA-256 Hash: {}", sha256sum);
-        // println!("MD5 Hash:     {}", md5sum);
+        display_stats(&stats, start.elapsed(), &outpath)?;
     }
+
+    Ok(())
+}
+
+fn display_stats(stats: &Stats, duration: Duration, outpath: &PathBuf) -> io::Result<()> {
+    let sudokus_rate = stats.puzzles as f64 / duration.as_secs_f64();
+    let avg_time = duration / stats.puzzles as u32;
+    let guess_rate = stats.guesses as f32 / stats.puzzles as f32;
+    let no_guess_percent = (stats.no_guesses as f32 / stats.puzzles as f32) * 100.0;
+
+    println!(
+        "Number of puzzles: {}",
+        stats.puzzles.to_formatted_string(&Locale::en)
+    );
+    println!(
+        "Time Taken: {:.2?}, Solve Rate: {}/s, Avg: {:.2?}",
+        duration,
+        (sudokus_rate as u32).to_formatted_string(&Locale::en),
+        avg_time
+    );
+    println!(
+        "No Guesses: {:.2}%, Guess Rate: {:.2} per puzzle",
+        no_guess_percent, guess_rate
+    );
+
+    let out_bytes = std::fs::read(outpath)?;
+    let sha256sum = crypto_hash::hex_digest(crypto_hash::Algorithm::SHA256, &out_bytes);
+    // let md5sum = crypto_hash::hex_digest(crypto_hash::Algorithm::MD5, &out_bytes);
+
+    println!("SHA-256 Hash: {}", sha256sum);
+    // println!("MD5 Hash:     {}", md5sum);
 
     Ok(())
 }
--- src/worker.rs
+++ src/worker.rs
@@ -1,10 +1,9 @@
-use std::thread;
-
 use crossbeam_channel::{Receiver, Sender};
+use std::thread;
 
 use crate::{
     consts::N_CELLS,
-    solver::{Solver, SolverBasic},
+    solver::{Solver, SolverBasic, Stats},
     sudoku::Sudoku,
 };
 
@@ -12,30 +11,28 @@ pub fn start_workers(
     num_threads: usize,
     read_rx: Receiver<(usize, Vec<u8>)>,
     write_tx: Sender<(usize, Vec<u8>)>,
-) -> Vec<thread::JoinHandle<usize>> {
+) -> Vec<thread::JoinHandle<Stats>> {
     (0..num_threads)
         .map(|_| {
             let rx = read_rx.clone();
             let tx = write_tx.clone();
             thread::spawn(move || {
                 let mut solver = SolverBasic::new(1, true);
-                let mut num_puzzles = 0;
+                let mut stats = Stats::new();
                 for (index, chunk) in rx {
-                    let (count, solved_chunk) = process_chunk(&mut solver, chunk);
+                    let solved_chunk = process_chunk(&mut solver, &mut stats, chunk);
                     tx.send((index, solved_chunk))
                         .expect("Error sending chunk to writer");
-                    num_puzzles += count;
                 }
-                num_puzzles
+                stats
             })
         })
         .collect()
 }
 
-fn process_chunk<S: Solver>(solver: &mut S, chunk: Vec<u8>) -> (usize, Vec<u8>) {
+fn process_chunk<S: Solver>(solver: &mut S, stats: &mut Stats, chunk: Vec<u8>) -> Vec<u8> {
     let mut solved_chunk = Vec::with_capacity(chunk.len() * 3);
 
-    let mut count = 0;
     for puzzle_slice in chunk.chunks_exact(N_CELLS + 1) {
         assert!(puzzle_slice[N_CELLS] == b'\n', "Invalid chunk format");
 
@@ -49,8 +46,12 @@ fn process_chunk<S: Solver>(solver: &mut S, chunk: Vec<u8>) -> (usize, Vec<u8>) 
         solved_chunk.extend_from_slice(solution.grid.as_ref());
         solved_chunk.push(b'\n');
 
-        count += 1;
+        stats.puzzles += 1;
+        if solver.guesses() == 0 {
+            stats.no_guesses += 1;
+        }
+        stats.guesses += solver.guesses();
     }
 
-    (count, solved_chunk)
+    solved_chunk
 }
